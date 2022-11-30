2a3c61663bd43a44b35ef17acf6678c12924d0c6
jesicayeu <119467018+jesicayeu@users.noreply.github.com>
2022-11-29 18:23:18 -0800

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,25 @@
+document.writeln("<CENTER>");
+document.writeln("<H2>Data Mahasiswa</H2>");
+document.writeln("<H2>Politeknik Amamapare Timika</H2>");
+document.writeln("<HR WIDTH=600 COLOR=Black");
+document.writeln("</CENTER>");
+
+
+// function tekan() 
+//                 {
+//                     var nimstr = (document.fform.nim.value);
+//                     var namastr = (document.fform.nama.value);
+//                     var jenisstr = (document.fform.jenis.value);
+//                     var agamastr = (document.fform.agama.value);
+//                     var statusstr = (document.fform.status.value);
+//                     var jurusanstr = (document.fform.jurusan.value);
+//                     var komentarstr = (document.fform.komentar.value);
+//                     document.fform.onim.value = nimstr;
+//                     document.fform.onama.value = namastr;
+//                     document.fform.ojenis.value = jenisstr;
+//                     document.fform.oagama.value = agamastr;
+//                     document.fform.ostatus.value = statusstr;
+//                     document.fform.ojurusan.value = jurusanstr;
+//                     document.fform.okomentar.value = komentarstr;
+//                 }
+
